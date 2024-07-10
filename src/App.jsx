175f34ad8c997ff60c99b01dd30886fedae21a03
Malesh Kumar <maleshkumar12345@gmail.com>
2024-07-10 19:34:59 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,45 @@
-import React from "react";
-import Header from "./components/Header";
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import MainItem from "./components/MainItem";
+import About from "./components/About";
+import { FaArrowUp } from "react-icons/fa";
 
-const App = () => {
+function App() {
+  const [showScroll, setShowScroll] = useState(false);
+
+  const checkScrollTop = () => {
+    if (!showScroll && window.pageYOffset > 400) {
+      setShowScroll(true);
+    } else if (showScroll && window.pageYOffset <= 400) {
+      setShowScroll(false);
+    }
+  };
+
+  const scrollTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  window.addEventListener("scroll", checkScrollTop);
   return (
-    <div className="w-full flex flex-col items-center justify-between">
-      {/**header section */}
-      <Header />
-      {/**main section */}
-      <MainItem />
-      {/**footer section */}
+    <div className="App">
+      <Navbar />
+      <Hero />
+      <About />
+      <Contact />
       <Footer />
+      {/* Scroll to Top Button */}
+      <button
+        className={`fixed bottom-4 right-4 bg-gray-500 text-white p-4 rounded-full shadow-md transition-opacity ${
+          showScroll ? "opacity-100" : "opacity-0"
+        }`}
+        onClick={scrollTop}
+      >
+        <FaArrowUp size={"24px"} />
+      </button>
     </div>
   );
-};
+}
 
 export default App;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Footer = () => {
-  return <div className="w-full h-20 py-5 z-10 shadow-md bg-blue-600"></div>;
+  return (
+    <footer className="border-t-2 shadow-lg">
+      {/* bg-gray-200 border-t-2 border-gray-300 */}
+      <div className="max-w-7xl mx-auto px-4 py-6">
+        <div className="text-sm text-gray-700 text-center">
+          <p>
+            COPYRIGHT © 2024 SHAURYA ELECTION MANAGEMENT - ALL RIGHTS RESERVED.
+          </p>
+          <p>DEVELOPED BY - MALESH KUMAR</p>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
